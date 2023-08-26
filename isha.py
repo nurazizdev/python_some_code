04fba566272f674c9f7f494b2cc57f382f429af4
nurazizdev <ungboyevn@gmail.com>
2023-08-26 19:34:14 +0500

    .

--- isha.py
+++ isha.py
@@ -1,28 +1,41 @@
-b=input()
-f=input()
-b=b.split(":")
-f=f.split(":")
-y=int(b[0])*3600+int(b[1])*60+int(b[2])-(int(f[0])*3600+int(f[1])*60+int(f[2]))
-
-x=abs(int(b[0])*3600+int(b[1])*60+int(b[2])-(int(f[0])*3600+int(f[1])*60+int(f[2])))
-if y>=0:
-    x=24*3600-x
-s1=x//3600
-s2=(x-s1*3600)//60
-s3=x-s1*3600-s2*60
-if s1<10 and s2>=10 and s3>=10:
-    print(f'0{s1}:{s2}:{s3}')
-elif s1<10 and s2<10 and s3>10:
-    print(f'0{s1}:0{s2}:{s3}')
-elif s1<10 and s2<10 and s3<10:
-    print(f'0{s1}:0{s2}:0{s3}')
-elif s1<10 and s2>=10 and s3<10:
-    print(f'0{s1}:{s2}:0{s3}')
-elif s1>=10 and s2<10 and s3<10:
-    print(f'{s1}:0{s2}:0{s3}')
-elif s1>=10 and s2>=10 and s3<10:
-    print(f'{s1}:{s2}:0{s3}')
-elif s1>=10 and s2<10 and s3>=10:
-    print(f'{s1}:0{s2}:{s3}')
-elif s1>=10 and s2>=10 and s3>=10:
-    print(f'{s1}:{s2}:{s3}')
+n,m=map(int,input().split())
+A=[]
+p=0
+for i in range(n):
+	a=list(map(str,input().split(None,m)[:m]))
+	A.append(a)
+for i in range(n):
+	for j in range(m):
+		if a[i][j]=="N":
+			h=0
+			for k in range(i):
+				if a[k][j]!=".":
+					h+=1
+			if h>0:
+				p+=1
+			a[i][j]=="."
+		if a[i][j]=="W":
+			h=0
+			for k in range(j):
+				if a[i][k]!=".":
+					h+=1
+			if h>0:
+				p+=1
+			a[i][j]=="."
+		if a[i][j]=="S":
+			h=0
+			for k in range(j+1,m):
+				if a[k][j]!=".":
+					h+=1
+			if h>0:
+				p+=1
+			a[i][j]=="."
+		if a[i][j]=="E":
+			h=0
+			for k in range(j):
+				if a[i][k]!=".":
+					h+=1
+			if h>0:
+				p+=1
+			a[i][j]=="."
+print(p)			
